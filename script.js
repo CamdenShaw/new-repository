27ed62f50a67f2e09d7e2488dbae29f6a97b6ed8
Camden S <camdens@ADION.local>
2020-06-08 10:17:43 -0700

    More examples for Nick.

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ $(document).ready(() => {
   $('h1').css({display: 'none', color: 'blue'})
 })
 
+function otherFunction() {
+  return new Promise(banana => {
+    setTimeout(() => {
+      banana(10 * 10)
+    }, 5000)
+  });
+}
+
 $(window).on('load', () => {
   $('h1').css({display: 'inline', color: 'pink'})
+
+  async function functionName () {
+    let something = await otherFunction()
+    let x = something * 10
+
+    console.log(something, x)
+  }
+
+  functionName()
 })
